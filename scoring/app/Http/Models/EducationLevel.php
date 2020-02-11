bf6688faefc3c8580474efaa6cc63cf05c614cf9
Stanislav <s.majevski@cointraffic.io>
2020-02-11 17:11:21 +0200

    Added: User Observer

--- scoring/app/Http/Models/EducationLevel.php
+++ scoring/app/Http/Models/EducationLevel.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EducationLevel extends Model
 {
-    //
+    /**
+     * The relationships between CountryTier and Country models.
+     *
+     * @return HasMany
+     */
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
---
+++ scoring/app/Observers/UserObserver.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\EducationLevel;
+use App\Models\EmailDomain;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "saved" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function saving(User $user)
+    {
+        $user->scoring = $this->calculateScoring($user);
+    }
+
+    private function calculateScoring($user)
+    {
+        return $this->calculateEmailScoring($user->email) + $this->calculateEducationScoring($user->education_id);
+
+    }
+
+    private function calculateEmailScoring($email)
+    {
+        $emailDomain = EmailDomain::whereName(substr($email, strpos($email, '@') + 1))->first();
+        return $emailDomain ? $emailDomain->scoring : EmailDomain::whereName('other')->first()->scoring;
+    }
+
+    private function calculateEducationScoring($education)
+    {
+        return EducationLevel::where($education)->first()->scoring;
+    }
+}
