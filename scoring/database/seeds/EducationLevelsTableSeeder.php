10eb70520cb198585087e5fef8d0f221717b1337
Stanislav <s.majevski@cointraffic.io>
2020-02-11 16:57:12 +0200

    Added: Education table seeder

---
+++ scoring/database/seeds/EducationLevelsTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\EducationLevel;
+use Illuminate\Database\Seeder;
+
+
+class EducationLevelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->educationLevels() as $educationLevel) {
+            EducationLevel::create($educationLevel);
+        }
+    }
+
+    private function educationLevels()
+    {
+        return [
+            [
+                'name' => 'higher',
+                'scoring' => 15,
+            ],
+            [
+                'name' => 'specialized secondary',
+                'scoring' => 10,
+            ],
+            [
+                'name' => 'secondary',
+                'scoring' => 5,
+            ],
+            [
+                'name' => 'other',
+                'scoring' => 0,
+            ],
+        ];
+    }
+
+}
