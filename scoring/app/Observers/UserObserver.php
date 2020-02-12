ab97ca84b0d3984057bea2b43d1f4ee4ef8b5cf2
Stanislav <s.majevski@cointraffic.io>
2020-02-12 16:22:10 +0200

    minor changes in user observer

--- scoring/app/Observers/UserObserver.php
+++ scoring/app/Observers/UserObserver.php
@@ -2,8 +2,6 @@
 
 namespace App\Observers;
 
-use App\Models\EducationLevel;
-use App\Models\EmailDomain;
 use App\Models\User;
 
 class UserObserver
@@ -16,22 +14,6 @@ class UserObserver
      */
     public function saving(User $user)
     {
-        $user->scoring = $this->calculateScoring($user);
-    }
-
-    private function calculateScoring($user)
-    {
-        return $this->calculateEmailScoring($user->email) + $this->calculateEducationScoring($user->education_id);
-    }
-
-    private function calculateEmailScoring($email)
-    {
-        $emailDomain = EmailDomain::whereName(substr($email, strpos($email, '@') + 1))->first();
-        return $emailDomain ? $emailDomain->scoring : EmailDomain::whereName('other')->first()->scoring;
-    }
-
-    private function calculateEducationScoring($education)
-    {
-        return EducationLevel::whereId($education)->first()->scoring;
+        $user->scoring = $user->calculateScoring();
     }
 }
