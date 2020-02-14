694b99f05f651a885343b5b40353da7eebb0ab3b
Stanislav <s.majevski@cointraffic.io>
2020-02-14 16:48:49 +0200

    Added: terms validation

--- scoring/app/Http/Requests/Users/StoreUserRequest.php
+++ scoring/app/Http/Requests/Users/StoreUserRequest.php
@@ -7,6 +7,14 @@ use Illuminate\Validation\Rule;
 
 class StoreUserRequest extends FormRequest
 {
+    protected function prepareForValidation()
+    {
+        if ($this->has('terms')) {
+            $this->merge(['terms'=> true]);
+        } else {
+            $this->merge(['terms'=> false]);
+        }
+    }
     /**
      * Get the validation rules that apply to the request.
      *
@@ -17,7 +25,7 @@ class StoreUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'surname' => 'required|string|max:255',
-            'phone' => 'required', //|integer|regex:/^79[0-9]{9}$/
+                'phone' => 'required|numeric|regex:/79[0-9]{9}/',
             'email' => ['required', 'string', 'email', 'max:255',
                 Rule::unique('users')->ignore($this->id),
             ],
