8a5271772a572e3c818f0a1fd98c584490b453c3
Stanislav <s.majevski@cointraffic.io>
2020-02-14 16:47:54 +0200

    Added: phone validation

--- scoring/resources/js/app.js
+++ scoring/resources/js/app.js
@@ -5,24 +5,6 @@ require('imask');
 $(function () {
     $('[data-toggle="tooltip"]').tooltip({ html: true });
     $("#phone").inputmask({"mask": "+7(\\999)999-99-99", removeMaskOnSubmit: true, onUnMask: function(maskedValue) {
-        return maskedValue.replace(/[^\w\s]/gi, '');
+        return maskedValue.replace(/[_\W]+/g, '');
     }});
-});
-
-
-$(document).on('change','#create-terms',function(){
-    if($(this).is(':checked')){
-       $('#create-terms').val(1);
-    }else{
-       $('#create-terms').val(0);
-    }
-});
-
-
-$(document).on('change','#edit-terms',function(){
-    if($(this).is(':checked')){
-       $('#edit-terms').val(1);
-    }else{
-       $('#edit-terms').val(0);
-    }
-});
+  });
