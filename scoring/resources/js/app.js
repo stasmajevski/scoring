6581cb44ff748fb9dba3eecbe1408b761c51d981
stasmaj <s.majevski@cointraffic.io>
2020-02-15 15:29:44 +0200

    Disabled button

--- scoring/resources/js/app.js
+++ scoring/resources/js/app.js
@@ -1,10 +1,22 @@
 require('./bootstrap');
 require('inputmask/lib/jquery.inputmask');
-require('imask');
 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip({ html: true });
-    $("#phone").inputmask({"mask": "+7(\\999)999-99-99", removeMaskOnSubmit: true, onUnMask: function(maskedValue) {
-        return maskedValue.replace(/[_\W]+/g, '');
-    }});
-  });
+    $('[data-toggle="tooltip"]').tooltip({
+        html: true
+    });
+
+    $("#phone").inputmask({
+        "mask": "+7(\\999)999-99-99",
+        removeMaskOnSubmit: true,
+        onUnMask: function (maskedValue) {
+            return maskedValue.replace(/[_\W]+/g, '');
+        }
+    });
+
+    $("button[type=submit]").attr("disabled", "disabled");
+
+    $('input').keypress(function () {
+        $("button[type='submit']").removeAttr('disabled');
+    });
+});
