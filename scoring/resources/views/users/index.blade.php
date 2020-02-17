6032a3361b17712265a4f1761a05179548282b47
Stanislav <s.majevski@cointraffic.io>
2020-02-17 10:23:24 +0200

    Added: minor fixes

--- scoring/resources/views/users/index.blade.php
+++ scoring/resources/views/users/index.blade.php
@@ -1,14 +1,13 @@
 @extends('base')
 @section('button')
-<a href="" class="btn btn-success" data-toggle="modal" data-target="#create"><i
-        class="material-icons">add_circle</i><span>Add User</span></a>
+    <a href="" class="btn btn-success" data-toggle="modal" data-target="#create"><i class="material-icons">add_circle</i><span>Add User</span></a>
 @endsection
-@include('users.create')
 @section('main')
-<div class="row">
-    <div class="col-sm-12">
-        <table class="table table-striped">
-            <thead>
+    @include('users.create')
+    <div class="row">
+        <div class="col-sm-12">
+            <table class="table table-striped">
+                <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
@@ -19,36 +18,36 @@
                     <th>Scoring</th>
                     <th>Action</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($users as $user)
-                <tr>
-                    <td>{{$user->id}}</td>
-                    <td>{{$user->name}} {{$user->surname}}</td>
-                    <td>{{$user->phone}}</td>
-                    <td>{{$user->email}}</td>
-                    <td>{{$user->education->name}}</td>
-                    <td><input type="checkbox" @if($user->terms) checked @endif disabled></td>
-                    <td class="info">
-                        {{$user->scoring}}
-                        <i class="material-icons info_outline" data-toggle="tooltip" data-html="true"
-                            data-placement="right" title="
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}} {{$user->surname}}</td>
+                        <td>{{$user->phone}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->education->name}}</td>
+                        <td><input type="checkbox" @if($user->terms) checked @endif disabled></td>
+                        <td class="info">
+                            {{$user->scoring}}
+                            <i class="material-icons info_outline" data-toggle="tooltip" data-html="true"
+                               data-placement="right" title="
                                 Phone: {{$user->phoneScoring}}</br>
                                 Email: {{$user->emailScoring}}</br>
                                 Education: {{$user->education->scoring}}</br>
                                 Terms: {{$user->termsScoring}}
-                               ">
-                            &#xe88f;
-                        </i>
-                    </td>
-                    <td>
-                        <a href="{{$user->id}}" class="btn btn-primary">Edit</a>
-                    </td>
-                </tr>
+                                    ">
+                                &#xe88f;
+                            </i>
+                        </td>
+                        <td>
+                            <a href="{{$user->id}}" class="btn btn-primary">Edit</a>
+                        </td>
+                    </tr>
                 @endforeach
-            </tbody>
-        </table>
-        {{ $users->links() }}
+                </tbody>
+            </table>
+            {{ $users->links() }}
+        </div>
     </div>
-</div>
 @endsection
