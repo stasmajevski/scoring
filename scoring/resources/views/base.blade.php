f087839b3471d8252de2c378286d6fae88b22f6d
Stanislav <s.majevski@cointraffic.io>
2020-02-17 10:16:17 +0200

    Added: material icons package

--- scoring/resources/views/base.blade.php
+++ scoring/resources/views/base.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users Scoring</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
